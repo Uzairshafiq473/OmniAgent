c1ea7c1f5d63cadb881d834d9673c97bf268fe0e
Muhammad Uzair Shafiq <uzairshafiq473@gmail.com>
2025-03-26 00:12:17 +0500

    push

--- frontend.py
+++ frontend.py
@@ -21,7 +21,7 @@ selected_model = st.selectbox("Select Model:", model_options[provider])
 allow_web_search = st.checkbox("Enable Web Search", value=True)
 user_query = st.text_area("Enter your query:", height=150, placeholder="Ask Anything!")
 
-API_URL = "http://127.0.0.1:9999/chat"
+API_URL = "https://OmniAgent.deta.dev/chat" 
 
 if st.button("Ask Agent!"):
     if user_query.strip():
